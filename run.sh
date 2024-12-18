e7c21283465442a4143eaa788b0604c9966ee223
biancco <biancco@snu.ac.kr>
2024-12-18 17:16:15 +0900

    env setup

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # 10 + 10 _ ft
-python tools/train_net.py --num-gpus 4 --config-file .configs/ft2024.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.001
+python tools/train_net.py --num-gpus 1 --config-file configs/ft2024.yaml SOLVER.IMS_PER_BATCH 8 SOLVER.BASE_LR 0.001
